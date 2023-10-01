2de2c483e9510a803e6aecebb7c9ac98a36bb066
Matthew Warman <warmanmr@gmail.com>
2023-10-01 08:29:33 -0400

    mysql and liquibase

--- src/main/java/net/leanstacks/todosvc/todo/Todo.java
+++ src/main/java/net/leanstacks/todosvc/todo/Todo.java
@@ -5,13 +5,14 @@ import java.io.Serializable;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Todo implements Serializable {
 
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   @Column(nullable = false)
