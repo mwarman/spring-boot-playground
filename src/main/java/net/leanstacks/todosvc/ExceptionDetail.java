0851c6ee2dab3b093e6b28d8337b71a0d2bfcb67
Matthew Warman <warmanmr@gmail.com>
2023-10-08 15:47:50 -0400

    add ExceptionDetail

---
+++ src/main/java/net/leanstacks/todosvc/ExceptionDetail.java
@@ -0,0 +1,33 @@
+package net.leanstacks.todosvc;
+
+import org.springframework.http.HttpStatus;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public class ExceptionDetail {
+
+  private HttpStatus httpStatus;
+
+  private String message;
+
+  public ExceptionDetail(HttpStatus httpStatus, String message) {
+    this.httpStatus = httpStatus;
+    this.message = message;
+  }
+
+  @Schema(example = "400")
+  public int getStatus() {
+    return this.httpStatus.value();
+  }
+
+  @Schema(example = "Status value description")
+  public String getStatusText() {
+    return this.httpStatus.getReasonPhrase();
+  }
+
+  @Schema(example = "Problem-specific message")
+  public String getMessage() {
+    return this.message;
+  }
+
+}
--- src/main/java/net/leanstacks/todosvc/TodoServiceExceptionHandler.java
+++ src/main/java/net/leanstacks/todosvc/TodoServiceExceptionHandler.java
@@ -8,19 +8,24 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import io.swagger.v3.oas.annotations.Hidden;
+
+@Hidden
 @RestControllerAdvice
 public class TodoServiceExceptionHandler {
 
   @ExceptionHandler(NoSuchElementException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
-  public ResponseEntity<String> handleNoSuchElement(RuntimeException ex) {
-    return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+  public ResponseEntity<?> handleNoSuchElement(RuntimeException ex) {
+    HttpStatus status = HttpStatus.NOT_FOUND;
+    return new ResponseEntity<>(new ExceptionDetail(status, ex.getMessage()), status);
   }
 
   @ExceptionHandler(RuntimeException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
-  public ResponseEntity<String> handleException(RuntimeException ex) {
-    return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+  public ResponseEntity<?> handleException(RuntimeException ex) {
+    HttpStatus status = HttpStatus.BAD_REQUEST;
+    return new ResponseEntity<>(new ExceptionDetail(status, ex.getMessage()), status);
   }
 
 }
