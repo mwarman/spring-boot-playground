837934afc0e948fca0b1b44cb5d9fa92d8e7a4b1
Matthew Warman <warmanmr@gmail.com>
2023-10-15 08:04:02 -0400

    endpoint authorization

--- src/main/java/net/leanstacks/todosvc/security/SecurityConfiguration.java
+++ src/main/java/net/leanstacks/todosvc/security/SecurityConfiguration.java
@@ -2,8 +2,13 @@ package net.leanstacks.todosvc.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,8 +16,34 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
   @Bean
+  public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+    // password encoding for demonstration purposes only
+    // not suitable for production
+    UserBuilder users = User.withDefaultPasswordEncoder();
+    UserDetails user = users
+        .username("user")
+        .password("userpass")
+        .roles("USER")
+        .build();
+    UserDetails admin = users
+        .username("admin")
+        .password("adminpass")
+        .roles("ADMIN")
+        .build();
+    return new InMemoryUserDetailsManager(user, admin);
+  }
+
+  @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf((csrf) -> csrf.disable());
+    http
+        .csrf((csrf) -> csrf.disable())
+        .authorizeHttpRequests(
+            (authorize) -> authorize
+                .requestMatchers("/api/todos/**").hasRole("USER")
+                .requestMatchers("/api/actuators/health").permitAll()
+                .requestMatchers("/api/actuators/**").hasRole("ADMIN")
+                .anyRequest().denyAll())
+        .httpBasic(Customizer.withDefaults());
 
     return http.build();
   }
